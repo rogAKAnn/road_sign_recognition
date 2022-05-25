62f9aa87ce0f08f6ac553ccdb7abe0eebdc1a693
Roger <nguyenvo1028rg@gmail.com>
2022-05-25 20:10:02 +0700

    Initial dataset

---
+++ DLModel/TrafficSignNet.py
@@ -0,0 +1,68 @@
+# Importing package
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dense
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+class TrafficSignNet:
+	@staticmethod
+	def build(width, height, depth, classes):
+		# Initialise the model along with the input shape to be 
+		# "Channel last" and the channel dimensions itself.
+		model = Sequential()
+		inputShape = (height, width, depth)
+		chanDim = -1
+
+		model.add(Conv2D(8, (5,5), padding="same", input_shape=inputShape))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(2,2)))
+
+		# first set of (CONV => RELU => CONV => RELU) * 2 => POOL
+		model.add(Conv2D(16, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(Conv2D(16, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+		# second set of (CONV => RELU => CONV => RELU) * 2 => POOL
+		model.add(Conv2D(32, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(Conv2D(32, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+	
+
+		# first set of FC => RELU layers
+		model.add(Flatten())
+		model.add(Dense(128))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization())
+		model.add(Dropout(0.5))
+		# second set of FC => RELU layers
+		model.add(Flatten())
+		model.add(Dense(128))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization())
+		model.add(Dropout(0.5))
+		# softmax classifier
+		model.add(Dense(classes))
+		model.add(Activation("softmax"))
+		# return the constructed network architecture
+		return model
+
+
+
+
+
+
